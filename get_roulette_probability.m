f91a9476bc2ed0d81092bf9a2c8dd68c03a5573c
Marcel Siwiela <118558793+Equerm8@users.noreply.github.com>
2024-11-20 22:56:33 +0100

    Update get_roulette_probability.m

--- get_roulette_probability.m
+++ get_roulette_probability.m
@@ -1,13 +1,13 @@
-function roulette_probability = get_roulette_probability(J)
+function roulette_probability = get_roulette_probability(J, N)
     sumation = sum(J);
-    involvement = zeros(30, 1);
-    for i = 1:30
+    involvement = zeros(N, 1);
+    for i = 1:N
         involvement(i) = sumation / J(i);
     end
     involv_sumation = sum(involvement);
     real_involvement = involvement / involv_sumation;
-    roulette_probability = zeros(30, 1);
-    for i = 1:30
+    roulette_probability = zeros(N, 1);
+    for i = 1:N
         roulette_probability(i) = sum(real_involvement(1:i)) * 100;
     end
 end
